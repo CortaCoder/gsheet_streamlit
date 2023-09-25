753cc80311e1611a25fbf4a15d3f6ae7c369a329
CortaCoder <68209016+CortaCoder@users.noreply.github.com>
2023-09-25 10:40:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,12 @@ conn = connect(credentials=credentials)
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    print(rows)
+    try:
+        rows = conn.execute(query, headers=1)
+        rows = rows.fetchall()
+        print(rows)
+    except Exception as e:
+        return [str(e),]
     return rows
 
 sheet_url = st.secrets["private_gsheets_url"]
@@ -24,5 +27,8 @@ print(sheet_url)
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
-for row in rows:
-    st.write(f"Log folder - {row.folder_name} has a file :{row.file_name}:")
+if len(rows)>1:
+    for row in rows:
+        st.write(f"Log folder - {row.folder_name} has a file :{row.file_name}:")
+else:
+    st.write(rows[0)
